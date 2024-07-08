bdd7ace7ec9d588d5d8ce73bda118cce946cfe80
r7projects-shayan <shayansworks@gmail.com>
2024-07-08 18:26:34 +0530

    day1

---
+++ course/Basic.java
@@ -0,0 +1,13 @@
+// Multithreading = multiple process execute at same time
+// Multitasking = running multiple apps in the same time
+
+// the class which has the main function should be of the same name as the filename
+
+// class has the first letter as capital
+
+
+class Basic{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
